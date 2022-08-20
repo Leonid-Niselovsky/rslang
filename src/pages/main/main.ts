8617f1ff3d185d4beaade50fc700d88e87e80e15
Leonid Niselovsky <leonisel@outlook.com>
2022-08-20 12:25:13 +0300

    add burger menu

--- src/pages/main/main.ts
+++ src/pages/main/main.ts
@@ -1,4 +1,21 @@
 import './main.scss';
 
-let container = document.querySelector('.container') as HTMLBodyElement;
-container.style.color = 'green';
+const burger = document.querySelector('#burger');
+const popup = document.querySelector('#popup');
+const menu = document.querySelector('#menu');
+const body = document.body;
+
+burger.addEventListener('click', showPopup);
+
+function showPopup(e) {
+  e.preventDefault();
+  popup.classList.toggle('open');
+  burger.classList.toggle('active');
+  body.classList.toggle('dontscroll');
+  renderPopupMenu();
+}
+
+function renderPopupMenu() {
+  popup.append(menu);
+}
+
