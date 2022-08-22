810989062ec5adc9fd78c9aa9b51d8d62fb955c2
Arina2246 <arinanebylova@gmail.com>
2022-08-22 18:56:20 +0200

    feat: add final page

--- src/pages/games/audioChalenge/audioChalenge.ts
+++ src/pages/games/audioChalenge/audioChalenge.ts
@@ -107,9 +107,6 @@ class AudioChalenge{
         for(let i=0;i<gameButtons.length;i++){
             const button=gameButtons[i];
             button.innerHTML=val[i].wordTranslate;
-            // if (button.innerHTML===rightWord){
-            //     rightWord=button
-            // }
             const clone = button.cloneNode(true) as Element;
             gameButtonsContainer.replaceChild(clone, button);
             if (clone.innerHTML===rightWord){
@@ -122,13 +119,6 @@ class AudioChalenge{
                       this.wrongWord(clone,rightWord, wordImage,wordInEnglich)
                   }
               })
-            // button.addEventListener('click',()=>{
-            //     if( button.innerHTML===Word.wordTranslate){
-            //         this.rightWord(button, wordImage,wordInEnglich)
-            //     }else{
-            //         this.wrongWord(button,rightWord, wordImage,wordInEnglich)
-            //     }
-            // })
         }
     })
   }
@@ -209,8 +199,6 @@ class AudioChalenge{
           if(button.innerHTML===wordTranslate){
             this.clickWord(wordImage,wordInEnglish)
             button.classList.add('rightWord')
-            // this.rightWord(button, wordImage,wordInEnglish)
-            this.resultArray.pop()
             this.resultArray.push(false)
           }     
         }
@@ -226,9 +214,64 @@ class AudioChalenge{
     if (this.counter<10){
       this.fillGamePage(this.counter)
     }else{
-      console.log(this.resultArray)
-      // final page
+      this.showFinalPage()
     }
+  }
+  showFinalPage(){
+    const finalPage=document.querySelector('.finalPage')
+    const gamePage=document.querySelector('.gameAudioChalenge')
+    gamePage.classList.add('displayNoneForGame')
+    finalPage.classList.remove('displayNoneForGame')
+    const rightWords=document.querySelector('.finalPageRight')
+    const wrongWords=document.querySelector('.finalPageWrong')
+    while (rightWords.firstChild) {
+      rightWords.removeChild(rightWords.firstChild);
+    }
+    while (wrongWords.firstChild) {
+    wrongWords.removeChild(wrongWords.firstChild);
+    }
+    for(let i=0;i<this.words.length;i++){
+      const result = this.resultArray[i]
+      const word = this.words[i]
+      if (result){
+        rightWords.insertAdjacentHTML('beforeend', this.fillFinalPage(word.word, word.wordTranslate));
+        const soundButton = rightWords.children[rightWords.children.length -1 ].children[0];
+        soundButton.addEventListener('click',()=>this.playAudioFinal(this.url + word.audio))
+      }else{
+        wrongWords.insertAdjacentHTML('beforeend', this.fillFinalPage(word.word, word.wordTranslate));
+        const soundButton = wrongWords.children[wrongWords.children.length -1 ].children[0];
+        soundButton.addEventListener('click',()=>this.playAudioFinal(this.url + word.audio))
+
+      }
+    }
+    const rightWordNodeList = rightWords.children.length;
+    const WrongWordNodeList = wrongWords.children.length;
+    const rightWordCount=`<p>Правильно: ${rightWordNodeList}</p>`
+    const wrongWordCount=`<p>Неправильно: ${WrongWordNodeList}</p>`
+    rightWords.insertAdjacentHTML('afterbegin', rightWordCount);
+    wrongWords.insertAdjacentHTML('afterbegin', wrongWordCount);
+  }
+  fillFinalPage(word: string,wordTranslate: string){
+    const element=`
+    <div>
+    <svg class='svgFinalPage' width="17px" height="17px" viewBox="0 0 17 17" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" class="si-glyph si-glyph-sound">
+    
+    <title>1193</title>
+    
+    <defs></defs>
+    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+        <path d="M13.987,14.868 C13.987,15.494 13.308,16 12.471,16 L5.957,11.473 C5.118,11.473 4.044,10.965 4.044,10.34 L4.044,5.682 C4.044,5.058 5.118,4.55 5.957,4.55 L12.471,0.022 C13.308,0.022 13.987,0.53 13.987,1.155 L13.987,14.868 L13.987,14.868 Z" fill="#434343" class="si-glyph-fill"></path>
+    </g>
+</svg>
+    <span>${word} - ${wordTranslate}</span>
+    </div>
+    `
+    return element;
+  }
+  playAudioFinal(soundUrl){
+    const audio = new Audio();
+    audio.src = soundUrl;
+    audio.autoplay = true;
   }
 }
 export default AudioChalenge;
