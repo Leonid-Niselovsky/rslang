0d8ec37e49dcaafb98ca677c9552294ad0df74e3
Leonid Niselovsky <leonisel@outlook.com>
2022-08-26 01:05:03 +0300

    feat: add layout authorization form

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,4 +1,27 @@
 import './main.scss';
+import ApiUsers from '../../api/apiUsers';
 
-let container = document.querySelector('.container') as HTMLBodyElement;
-container.style.color = 'green';
+// FORM ANIMATION
+
+const signInBtn = document.querySelector('.signin_btn') as HTMLBodyElement;
+const signUpBtn = document.querySelector('.signup_btn') as HTMLBodyElement;
+const form = document.querySelector('.form') as HTMLBodyElement;
+const body = document.body;
+
+signUpBtn.addEventListener('click', function () {
+  form.classList.add('active');
+  body.classList.add('active');
+});
+
+signInBtn.addEventListener('click', function () {
+  form.classList.remove('active');
+  body.classList.remove('active');
+});
+
+// Registration
+const formSignUp = document.querySelector('.form-wrapper_signup') as HTMLBodyElement;
+const login = document.querySelector('.reg-login') as HTMLBodyElement;
+const email = document.querySelector('.reg-email') as HTMLBodyElement;
+const password = document.querySelector('.reg-password') as HTMLBodyElement;
+
+formSignUp.addEventListener('submit', function())
