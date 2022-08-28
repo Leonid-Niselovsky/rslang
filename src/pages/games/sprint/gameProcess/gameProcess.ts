253bc846a3f887ed044cfea9a3cb8ca520935f7b
Arina2246 <arinanebylova@gmail.com>
2022-08-28 17:26:49 +0200

    feat: add points for game

--- src/pages/games/sprint/gameProcess/gameProcess.ts
+++ src/pages/games/sprint/gameProcess/gameProcess.ts
@@ -2,10 +2,14 @@ class GameProcess{
     interval: any;
     counter: number;
     resultArray: any[];
+    animationResult: HTMLBodyElement;
+    arrForFinalPage: any[];
     constructor(){
         this.counter = -1;
         this.interval;
-        this.resultArray=[]
+        this.resultArray=[];
+        this.animationResult=document.querySelector('.resultAnimation')as HTMLBodyElement
+        this.arrForFinalPage=[];
     }
     showFirstPage(arr){
         if (arr.length!==0){
@@ -34,22 +38,29 @@ class GameProcess{
             if(button === 'ArrowLeft'){
                 this.fillGamePage(arr,false)
             }
-            if(button === 'ArrowRight'){
+            if(button === 'ArrowRight'){ 
                 this.fillGamePage(arr,true)
             }
         })
     }
     }
     fillGamePage(arr,result){
-        this.counter+=1;
+        this.animationResult.classList.remove('resultAnimationdd')
+        window.setTimeout(()=>this.animationResult.classList.add('resultAnimationdd'),500)
         this.resultArray.push(result)
+        if((result ===true||result ===false)&&(arr[this.counter].result===this.resultArray[this.counter])){
+            this.rightWord()
+        }else if((result ===true||result ===false)&&(arr[this.counter].result!==this.resultArray[this.counter])){
+            this.wrongWord()
+        }
+        this.counter+=1;
         const word=arr[this.counter].word
         const wordGuess=arr[this.counter].guessWord
         const wordElement=document.querySelector('.wordForSprint')
         const wordElementGuess=document.querySelector('.wordTranslateForSprint')
         wordElement.innerHTML=word;
         wordElementGuess.innerHTML=wordGuess;
-        console.log(this.resultArray)
+        const elem=document.querySelector('.resultAnimation') as HTMLBodyElement
     }
     timer(){
         const timer =document.querySelector('.timerForGame')
@@ -63,6 +74,69 @@ class GameProcess{
     }
     stopGame(){
         clearInterval(this.interval)
+    }
+    rightWord(){
+        this.arrForFinalPage.push(true)
+        this.animationResult.innerHTML='Right'
+        this.animationResult.style.color='green'
+        this.pointsCounter()
+        const pointsForGame=document.querySelector('.pointsSprint');
+       const pointsForWord=document.querySelector('.pointsForWordSprint');
+       const points=Number(pointsForWord.innerHTML.slice(29, 31))
+       const pointsAtAll=Number(pointsForGame.innerHTML.slice(26, pointsForGame.innerHTML.length))
+       pointsForGame.innerHTML=`<span>Очков всего: </span>${pointsAtAll+points}`
+    }
+    wrongWord(){
+        this.arrForFinalPage.push(false)
+        const pointsForGame=document.querySelectorAll('.pointRightWordSprint');
+        const pointsForWord=document.querySelector('.pointsForWordSprint');
+        pointsForGame[0].classList.remove('pointRightWordSprintRight')
+        pointsForGame[1].classList.remove('pointRightWordSprintRight')
+        pointsForGame[2].classList.remove('pointRightWordSprintRight')
+        pointsForWord.innerHTML='<span>Очков за слово: </span>10'
+        this.animationResult.innerHTML='Wrong'
+        this.animationResult.style.color='red'
+    }
+    pointsCounter(){
+        const pointsForGame=document.querySelectorAll('.pointRightWordSprint');
+        const pointsForWord=document.querySelector('.pointsForWordSprint');
+    let firstBlock=false;
+    let secondBlock=false;
+        if(this.arrForFinalPage[this.arrForFinalPage.length-1]===true){
+            pointsForGame[0].classList.add('pointRightWordSprintRight')
+            if(this.arrForFinalPage[this.arrForFinalPage.length-2]===true){
+              pointsForGame[1].classList.add('pointRightWordSprintRight')
+              if(this.arrForFinalPage[this.arrForFinalPage.length-3]===true){
+                  pointsForGame[2].classList.add('pointRightWordSprintRight')
+                  if(this.arrForFinalPage[this.arrForFinalPage.length-4]===true){
+                      pointsForWord.innerHTML=`<span>Очков за слово: </span>20`
+                      pointsForGame[0].classList.remove('pointRightWordSprintRight')
+                      pointsForGame[1].classList.remove('pointRightWordSprintRight')
+                      pointsForGame[2].classList.remove('pointRightWordSprintRight')  
+                      firstBlock=true;
+          }}}}
+          if(this.arrForFinalPage[this.arrForFinalPage.length-5]===true&&firstBlock){
+              pointsForGame[0].classList.add('pointRightWordSprintRight')
+              if(this.arrForFinalPage[this.arrForFinalPage.length-6]===true){
+                pointsForGame[1].classList.add('pointRightWordSprintRight')
+                if(this.arrForFinalPage[this.arrForFinalPage.length-7]===true){
+                    pointsForGame[2].classList.add('pointRightWordSprintRight')
+                    if(this.arrForFinalPage[this.arrForFinalPage.length-8]===true){
+                      pointsForWord.innerHTML=`<span>Очков за слово: </span>40`
+                      pointsForGame[0].classList.remove('pointRightWordSprintRight')
+                      pointsForGame[1].classList.remove('pointRightWordSprintRight')
+                      pointsForGame[2].classList.remove('pointRightWordSprintRight')
+                      secondBlock=true  
+          }}}}
+          if(this.arrForFinalPage[this.arrForFinalPage.length-9]===true&&secondBlock){
+              pointsForGame[0].classList.add('pointRightWordSprintRight')
+              if(this.arrForFinalPage[this.arrForFinalPage.length-10]===true){
+                  pointsForGame[1].classList.add('pointRightWordSprintRight')
+                  if(this.arrForFinalPage[this.arrForFinalPage.length-11]===true){
+                      pointsForGame[2].classList.add('pointRightWordSprintRight')
+                      if(this.arrForFinalPage[this.arrForFinalPage.length-12]===true){
+                          pointsForWord.innerHTML=`<span>Очков за слово: </span>80`
+          }}}}
     }
 }
 export default GameProcess;
