caeb3ad5831a1d3e4eae7fac4778a76df20dce47
pashasha <smogida1337@gmail.com>
2022-09-03 20:19:49 +0300

    fix: change games links

--- src/pages/dictionary/gameLink.ts
+++ src/pages/dictionary/gameLink.ts
@@ -1,8 +1,8 @@
 export function gameLink(title: string){
   return `
-      <a class="game-link">
+      <a class="game-link" href="${title.toLowerCase() !== 'sprint' ? './games.html' : './sprint.html'}">
         <h3 class="game-link-title">${title}</h3>
-        <img src="../../assets/img/${title.split(' ').join('').toLowerCase( )}.png">
+        <img src="../../assets/img/${title.split(' ').join('').toLowerCase()}.png">
       </a>
   `
 }
